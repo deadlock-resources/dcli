37edb21e464f3e83c487ea587760a0d5be3310a7
Alex <apuret@e-biz.fr>
2019-10-18 11:07:07 +0200

    docs: complete information given

--- dcli/template/java/src/main/java/app/Test.java
+++ dcli/template/java/src/main/java/app/Test.java
@@ -6,6 +6,7 @@ public class Test {
 
     public static void main(String[] args) {
         try {
+            // just run user code
             {{ targetFile }}.{{ targetMethod }};
         } catch (RuntimeException e) {
             Logger.logException(e);
