65376f9d447005241934fd08091863d018c94384
Alex <apuret@e-biz.fr>
2019-10-18 12:08:45 +0200

    feat: add solve runner, fix some java template

--- dcli/cli.py
+++ dcli/cli.py
@@ -4,20 +4,16 @@ import fire
 import inquirer
 import subprocess
 
-from .runner import Runner
 from .generatorCli import Generator
 from .logger import info
-
-class Cli(object):
-  """Deadlock CLI."""
-
-  def __init__(self):
-    self.gen = Generator()
-
+from .runner import run, solve
 
 def main():
-  fire.Fire(Cli)
-  # fire.Fire(Runner)
+  fire.Fire({
+    'gen': Generator(),
+    'solve': solve,
+    'run': run
+  })
 
 if __name__ == '__main__':
   main()
