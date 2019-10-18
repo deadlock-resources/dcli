99f4bad2782797c42a8ac456cb43b8aaceb2b447
Alex <apuret@e-biz.fr>
2019-10-18 12:17:44 +0200

    docs: complete man information for runner

--- dcli/runner.py
+++ dcli/runner.py
@@ -1,14 +1,27 @@
 from .logger import info
 import os
 
-def run(self, path='.'):
-    pass
+def run(path='.'):
+    '''
+    Run the mission under given path.
+    As if user clicked on Run button.
+
+    :param path: path of your mission. Default is .
+    '''
+    info('Building mission..')
+    os.system('docker build ' + path + ' -q -t c')
+    info('Running mission..')
+    os.system('docker run c run')
+    return ''
 
 
-def solve(self, path='.'):
-    """Run mission under given path.
-    default path: ./
-    """
+def solve(path='.'):
+    '''
+    Run Solve step for the mission under given path.
+    As if user clicked on Submit button.
+
+    :param path: path of your mission. Default is .
+    '''
     info('Building mission..')
     os.system('docker build ' + path + ' -q -t c')
     info('Running mission..')
