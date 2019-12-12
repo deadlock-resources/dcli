5e85c7638719f7708a605af2353d77e1c5a336a0
Slayug <bug.graph@gmail.com>
2019-12-12 16:53:26 +0100

    feat: add version command

--- dcli/cli.py
+++ dcli/cli.py
@@ -1,8 +1,8 @@
 #! python3
 import os
 import fire
-import inquirer
 import subprocess
+import pkg_resources  # part of setuptools
 
 from .generatorCli import Generator
 from .logger import info
@@ -10,12 +10,15 @@ from .runner import run, solve
 from .spinCursor import SpinCursor
 import time
 
+def version():
+  return pkg_resources.require("dcli")[0].version
 
 def main():
   fire.Fire({
+    'version': version,
     'gen': Generator(),
     'solve': solve,
-    'run': run
+    'run': run,
   })
 
 
