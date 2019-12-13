84ddeaef1901d32c69522f04ae1d6a6714fb6eb1
Slayug <bug.graph@gmail.com>
2019-12-13 16:30:59 +0100

    feat: run hack mission

--- dcli/generator/file.py
+++ dcli/generator/file.py
@@ -1,9 +1,18 @@
 import pkg_resources
+import yaml
+import io
 
+
+def getPathFromRoot(path):
+    return pkg_resources.resource_filename('dcli', '/' + path)
 
 def getPathFromTemplateDir(type, path):
     resource_path = '/'.join(('template', type + '/' + path))
     return pkg_resources.resource_filename('dcli', resource_path)
+
+def loadYaml(path):
+    with open(path, 'r') as stream:
+        return yaml.safe_load(stream)
 
 
 def loadFile(type, path):
