e7e5ce4182c020d7b48f20153f504e9637de58cd
Slayug <bug.graph@gmail.com>
2020-02-27 12:25:38 +0100

    build: auto install building tools

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+python3 -m pip install --upgrade pip setuptools wheel --user
+python3 -m pip install twine --user
+
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
