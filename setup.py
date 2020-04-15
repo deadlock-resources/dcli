3dadb14fbdbd3b66269f46c2c00ddf9c70110941
Slayug <bug.graph@gmail.com>
2020-04-15 17:59:44 +0200

    build: 1.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="deadlock-cli",
-    version="1.1.1",
+    version="1.1.2",
     author="Slayug",
     author_email="apuret@takima.fr",
     description="CLI tools for Deadlock challenges.",
