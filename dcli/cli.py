005e1fd5a7cd7898aff1f3b8106bd726bd61f72c
Kevin TANG <ktang@takima.fr>
2020-05-14 17:57:20 +0200

    chore: refacto language generation for dcli

--- dcli/cli.py
+++ dcli/cli.py
@@ -4,7 +4,7 @@ import fire
 import subprocess
 import pkg_resources  # part of setuptools
 
-from .generatorCli import Generator
+from .generatorCli import Generator, generate_for_language
 from .logger import info
 from .runner import run, solve
 from .spinCursor import SpinCursor
@@ -13,10 +13,19 @@ import time
 def version():
   return pkg_resources.require("deadlock-cli")[0].version
 
+def gen_without_type():
+  Generator().generate()
+
+def gen_with_type(l=''):
+  if l == '':
+    gen_without_type()
+  else:
+    generate_for_language(l)
+
 def main():
   fire.Fire({
     'version': version,
-    'gen': Generator(),
+    'gen': gen_with_type,
     'solve': solve,
     'run': run,
   })
