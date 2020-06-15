160787712a7d7729ea12a9e9c94af01b5d088999
Josquin Cornec <jcornec@takima.fr>
2020-06-15 14:45:22 +0200

    feat: download runner on build option

--- dcli/cli.py
+++ dcli/cli.py
@@ -1,9 +1,11 @@
 #! python3
+import os
+
 import fire
 import pkg_resources  # part of setuptools
 
 from .generatorCli import Generator, generate_for_language
-from .runner import run, solve
+from .runner import run, solve, download_if_necessary
 
 def version():
   return pkg_resources.require("deadlock-cli")[0].version
@@ -17,12 +19,16 @@ def gen_with_type(l=''):
   else:
     generate_for_language(l)
 
+def build(path=os.getcwd()):
+    download_if_necessary(path);
+
 def main():
   fire.Fire({
     'version': version,
     'gen': gen_with_type,
     'solve': solve,
     'run': run,
+    'build': build,
   })
 
 
