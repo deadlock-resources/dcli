55321faad35e9a5b25592288791f06a30b52f9fe
Slayug <bug.graph@gmail.com>
2020-07-03 18:05:42 +0200

    WIP

--- dcli/cli.py
+++ dcli/cli.py
@@ -1,11 +1,17 @@
 #! python3
 import os
+import json
+import urllib.request
 
 import fire
 import pkg_resources  # part of setuptools
+from distutils.version import StrictVersion
 
 from .generatorCli import Generator, generate_for_language
 from .runner import run, solve, download_if_necessary
+
+from .logger import info
+from colored import fg, bg, attr
 
 def version():
   return pkg_resources.require("deadlock-cli")[0].version
@@ -20,9 +26,25 @@ def gen_with_type(l=''):
     generate_for_language(l)
 
 def build(path=os.getcwd()):
-    download_if_necessary(path);
+    download_if_necessary(path)
+
+def last_version():
+    url = "https://pypi.org/pypi/deadlock-cli/json"
+    with urllib.request.urlopen(url) as response:
+      data = json.loads(response.read())
+      versionKeys = data["releases"].keys()
+      versionsList = list(versionKeys)
+      return versionsList[-1]
+
+def warning_if_outdated():
+  last = last_version()
+  current = version()
+  if (last == current):
+    info(f'{attr(0)}New version available [{fg(1)}{current}{attr(0)} -> {fg(2)}{last}{attr(0)}], run "pip install deadlock-cli --upgrade" to upgrade.')
+
 
 def main():
+  warning_if_outdated()
   fire.Fire({
     'version': version,
     'gen': gen_with_type,
